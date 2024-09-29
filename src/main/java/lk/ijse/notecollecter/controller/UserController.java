7acc1e533fbe56d150bf2620bfad3c17bfd8d1d4
pathumck <ckpathumck@gmail.com>
2024-09-29 23:57:41 +0530

    Tested get selected user

--- src/main/java/lk/ijse/notecollecter/controller/UserController.java
+++ src/main/java/lk/ijse/notecollecter/controller/UserController.java
@@ -5,10 +5,7 @@ import lk.ijse.notecollecter.service.UserService;
 import lk.ijse.notecollecter.util.AppUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -53,6 +50,14 @@ public class UserController {
         buildUserDTO.setPassword(password);
         buildUserDTO.setProfilePic(base64ProPic);
 
+        userService.saveUser(buildUserDTO);
+
         return buildUserDTO;
+    }
+
+    @GetMapping(value = "/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public UserDTO getSelectedUser(@PathVariable("userId") String userId) {
+        System.out.println("get"+userId);
+        return userService.getUser(userId);
     }
 }
--- src/main/java/lk/ijse/notecollecter/service/UserServiceImpl.java
+++ src/main/java/lk/ijse/notecollecter/service/UserServiceImpl.java
@@ -1,14 +1,18 @@
 package lk.ijse.notecollecter.service;
 
+import jakarta.transaction.Transactional;
 import lk.ijse.notecollecter.dao.UserDAO;
 import lk.ijse.notecollecter.dto.impl.UserDTO;
+import lk.ijse.notecollecter.entity.impl.UserEntity;
 import lk.ijse.notecollecter.util.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Component
+@Service
+@Transactional
 public class UserServiceImpl implements UserService{
     @Autowired
     private UserDAO userDAO;
@@ -28,7 +32,9 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public UserDTO getUser(String userId) {
-        return null;
+        UserEntity selectedUser = userDAO.getReferenceById(userId);
+        System.out.println(selectedUser);
+        return mapping.toUserDTO(selectedUser);
     }
 
     @Override
